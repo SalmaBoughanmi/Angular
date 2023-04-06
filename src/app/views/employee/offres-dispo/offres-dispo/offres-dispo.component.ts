bbdb7198ad48531326f9bb23452fb366798284a1
SalmaBoughanmi <129304685+SalmaBoughanmi@users.noreply.github.com>
2023-04-06 14:32:09 +0200

    changes

---
+++ src/app/views/employee/offres-dispo/offres-dispo/offres-dispo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-offres-dispo',
+  templateUrl: './offres-dispo.component.html',
+  styleUrls: ['./offres-dispo.component.css']
+})
+export class OffresDispoComponent {
+
+}
