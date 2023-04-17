93934008009fe66a447e7bfa222457b85ccbd76e
SalmaBoughanmi <129304685+SalmaBoughanmi@users.noreply.github.com>
2023-04-17 04:10:55 +0200

    changes

--- src/app/views/front/login/login/login.component.ts
+++ src/app/views/front/login/login/login.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 // function
+loginUsers: any [] = [];
+loginObj: any = {
+  email: '',
+  password:''
+}
 
+constructor() {}
 
-
+ngOnInit(): void {
 
 }
+ onLogin(){
+  debugger
+  const isUserExist = this.loginUsers.find(m => m.email == this.loginObj.email && m.password == this.loginObj);
+   if (isUserExist != undefined){
+    alert('Connexion avec succès');
+   }
+  else {
+    alert('La connexion a échoué')
+  }
+   }
+ }
